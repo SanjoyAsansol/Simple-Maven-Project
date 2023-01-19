f751d16f7c19fbba5c65a1b6db1fe25cca7477c2
lenovo <lenovo@DESKTOP-QQJG5DE>
2023-01-19 14:43:31 +0530

    Hello from test1 and Demo Package

---
+++ src/test/java/DemoBranch/Test1.java
@@ -0,0 +1,11 @@
+package DemoBranch;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test
+	public void test1() {
+		System.out.println("Hello from test1 and Demo Package");
+	}
+
+}
